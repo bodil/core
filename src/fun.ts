1532d66d6c493ee38be35bf4644ea21cc1ee5e0b
Bodil Stokke <17880+bodil@users.noreply.github.com>
2025-06-10 09:56:52 +0100

    Let's just finally add the identity function.

--- src/fun.ts
+++ src/fun.ts
@@ -4,3 +4,10 @@
 export function always<A>(value: A): () => A {
     return () => value;
 }
+
+/**
+ * The identity function: it just returns its argument unchanged.
+ */
+export function id<A>(value: A): A {
+    return value;
+}
